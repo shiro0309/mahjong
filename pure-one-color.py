96711a542cbb58e431dfe1718bd035e39f194864
Eri Akiyama <eriakiyama@microsoft.com>
2023-07-18 14:37:49 +0900

    complete 0718

--- pure-one-color.py
+++ pure-one-color.py
@@ -1,48 +1,96 @@
-run = [[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9]]
-triples = [[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5],[6,6,6],[7,7,7],[8,8,8],[9,9,9]]
-heads = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[9,9]]
-# all で二つ以上の数があればそれを雀頭として使う
-for head in range(1,10):
-    all = [1] * 4 + [2] * 4 + [3] * 4 + [4] * 4 + [5] * 4 + [6] * 4 + [7] * 4 + [8] * 4 + [9] * 4
-    if all.count(head) >= 2:
-        print("head:[{},{}]".format(head,head))
-        all.remove(head)
-        all.remove(head)
-        # 雀頭を除いた部分
-        # print(all)
-        for triple_1 in range(1,10):
-            answer = []
-            if all.count(triple_1) >= 3:
-                answer.append(triples[triple_1 - 1])
-                all_1 = all.copy()
-                all_1.remove(triple_1)
-                all_1.remove(triple_1)
-                all_1.remove(triple_1)
-                for triple_2 in range(triple_1+1,10):
-                    if all.count(triple_2) >= 3:
+
+def get_unique_list(seq):
+    seen = []
+    return [x for x in seq if x not in seen and not seen.append(x)]
+if __name__ == "__main__":
+    runs = [[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9]]
+    triples = [[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5],[6,6,6],[7,7,7],[8,8,8],[9,9,9]]
+    runs_triples = [[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9],[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5],[6,6,6],[7,7,7],[8,8,8],[9,9,9]]
+    heads = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[9,9]]
+    all_pure_one_color = []
+    # all で二つ以上の数があればそれを雀頭として使う
+    for head in heads:
+        all = [1] * 4 + [2] * 4 + [3] * 4 + [4] * 4 + [5] * 4 + [6] * 4 + [7] * 4 + [8] * 4 + [9] * 4
+        if all.count(head[0]) >= 2:
+            print("head:{}".format(head))
+            all.remove(head[0])
+            all.remove(head[0])
+            # 雀頭を除いた部分
+            for element in runs_triples:
+                answer = []
+                if (element[0] != element[1] and all.count(element[0]) >= 1 and all.count(element[1]) >= 1 and all.count(element[2]) >= 1 ) \
+                or (element[0] == element[1] and all.count(element[0]) >= 3):
+                    answer.append(element)
+                    all_1 = all.copy()
+                    all_1.remove(element[0])
+                    all_1.remove(element[1])
+                    all_1.remove(element[2])
+                    # print("answer:{}".format(answer))
+                    # print("all_1:{}".format(all_1))
+                    for element_2 in runs_triples:
                         answer_2 = answer.copy()
-                        all_2 = all_1.copy()
-                        answer_2.append(triples[triple_2 -1])
-                        all_2.remove(triple_2)
-                        all_2.remove(triple_2)
-                        all_2.remove(triple_2)
-                        for triple_3 in range(triple_2+1,10):
-                            if all.count(triple_3) >= 3:
+                        if (element_2[0] != element_2[1] and all_1.count(element_2[0]) >= 1 and all_1.count(element_2[1]) >= 1 and all_1.count(element_2[2]) >= 1 ) \
+                        or (element_2[0] == element_2[1] and all_1.count(element_2[0]) >= 3):
+                            answer_2.append(element_2)
+                            all_2 = all_1.copy()
+                            all_2.remove(element_2[0])
+                            all_2.remove(element_2[1])
+                            all_2.remove(element_2[2])
+                            for element_3 in runs_triples:
                                 answer_3 = answer_2.copy()
-                                all_3 = all_2.copy()
-                                answer_3.append(triples[triple_3 - 1])
-                                all_3.remove(triple_3)
-                                all_3.remove(triple_3)
-                                all_3.remove(triple_3)
-                                for triple_4 in range(triple_3+1,10):
-                                    if all.count(triple_4) >= 3:
+                                if (element_3[0] != element_3[1] and all_2.count(element_3[0]) >= 1 and all_2.count(element_3[1]) >= 1 and all_2.count(element_3[2]) >= 1 ) \
+                                or (element_3[0] == element_3[1] and all_2.count(element_3[0]) >= 3):
+                                    answer_3.append(element_3)
+                                    all_3 = all_2.copy()
+                                    all_3.remove(element_3[0])
+                                    all_3.remove(element_3[1])
+                                    all_3.remove(element_3[2])
+                                    for element_4 in runs_triples:
                                         answer_4 = answer_3.copy()
-                                        all_4 = all_3.copy()
-                                        answer_4.append(triples[triple_4 - 1])
-                                        all_4.remove(triple_4)
-                                        all_4.remove(triple_4)
-                                        all_4.remove(triple_4)
-                                        print(answer_4)
+                                        if (element_4[0] != element_4[1] and all_3.count(element_4[0]) >= 1 and all_3.count(element_4[1]) >= 1 and all_3.count(element_4[2]) >= 1 ) \
+                                        or (element_4[0] == element_4[1] and all_3.count(element_4[0]) >= 3):
+                                            answer_4.append(element_4)
+                                            all_4 = all_3.copy()
+                                            all_4.remove(element_4[0])
+                                            all_4.remove(element_4[1])
+                                            all_4.remove(element_4[2])
+                                            # print("answer:{}".format(answer_4))
+                                            # print("all_4:{}".format(all_4))
+                                            answer_4.append(head)
+                                            a = sum(answer_4,[])
+                                            a.sort()
+                                            all_pure_one_color.append(a)
+unique_list = get_unique_list(all_pure_one_color)
+print(unique_list)
+                # for triple_2 in range(triple_1+1,10):
+                #     if all.count(triple_2) >= 3:
+                #         answer_2 = answer.copy()
+                #         all_2 = all_1.copy()
+                #         answer_2.append(triples[triple_2 -1])
+                #         all_2.remove(triple_2)
+                #         all_2.remove(triple_2)
+                #         all_2.remove(triple_2)
+                # for run_1 in runs:
+                #     if all.count(run_1[0]) >= 1 and all.count(run_1[1]) >= 1 and all.count(run_1[1]) >= 1:
+                #         answer_2 = answer.copy()
+                #         all_2 = all_1.copy()
+                        # for triple_3 in range(triple_2+1,10):
+                        #     if all.count(triple_3) >= 3:
+                        #         answer_3 = answer_2.copy()
+                        #         all_3 = all_2.copy()
+                        #         answer_3.append(triples[triple_3 - 1])
+                        #         all_3.remove(triple_3)
+                        #         all_3.remove(triple_3)
+                        #         all_3.remove(triple_3)
+                        #         for triple_4 in range(triple_3+1,10):
+                        #             if all.count(triple_4) >= 3:
+                        #                 answer_4 = answer_3.copy()
+                        #                 all_4 = all_3.copy()
+                        #                 answer_4.append(triples[triple_4 - 1])
+                        #                 all_4.remove(triple_4)
+                        #                 all_4.remove(triple_4)
+                        #                 all_4.remove(triple_4)
+                        #                 print(answer_4)
                                         
                            
         
