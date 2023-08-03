75d368909ac957db925f98e14d65083aa6200f2a
Eri Akiyama <eriakiyama@microsoft.com>
2023-08-03 14:54:59 +0900

    鳴きの数と枚数を指定可能に変更

--- pure-one-color.py
+++ pure-one-color.py
@@ -1,16 +1,48 @@
+import itertools
+
+# 鳴いている形を指定
+calling = [[1,2,3],[2,3,4]]
+# そのほかで使われている枚数をそれぞれ指定
+one = 0
+two = 0
+three = 0
+four = 0
+five = 0
+six = 0
+seven = 0
+eight = 0
+nine = 0
+#######################################################################
+
+# 鳴いている形をすべて合体
+calling = list(itertools.chain.from_iterable(calling))
+# 何メンツ鳴いているか
+calling_num = len(calling)
+print("calling-num:{}".format(calling_num))
+
+# それぞれの枚数で使える数を計算
+one_remain = 4 - calling.count(1) - one
+two_remain = 4 - calling.count(2) - two
+three_remain = 4 - calling.count(3) - three
+four_remain = 4 - calling.count(4) - four
+five_reamin = 4 - calling.count(5) - five
+six_remain = 4 - calling.count(6) - six
+seven_remain = 4 - calling.count(7) - seven
+eight_remain = 4 - calling.count(8) - eight
+nine_remain = 4 - calling.count(9) - nine
 
 def get_unique_list(seq):
     seen = []
     return [x for x in seq if x not in seen and not seen.append(x)]
 if __name__ == "__main__":
-    runs = [[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9]]
-    triples = [[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5],[6,6,6],[7,7,7],[8,8,8],[9,9,9]]
     runs_triples = [[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9],[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5],[6,6,6],[7,7,7],[8,8,8],[9,9,9]]
     heads = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[9,9]]
     all_pure_one_color = []
-    # all で二つ以上の数があればそれを雀頭として使う
+    # 面子手
     for head in heads:
-        all = [1] * 4 + [2] * 4 + [3] * 4 + [4] * 4 + [5] * 4 + [6] * 4 + [7] * 4 + [8] * 4 + [9] * 4
+        all = [1] * one_remain + [2] * two_remain + [3] * three_remain + [4] * four_remain + [5] * five_reamin + [6] * six_remain + [7] * seven_remain + [8] * eight_remain + [9] * nine_remain 
+        print("1:{}枚, 2:{}枚, 3:{}枚, 4:{}枚, 5:{}枚, 6:{}枚, 7:{}枚, 8:{}枚, 9:{}枚".format(one_remain, two_remain, three_remain, four_remain, five_reamin, six_remain, seven_remain, eight_remain, nine_remain))
+        quit()
         if all.count(head[0]) >= 2:
             print("head:{}".format(head))
             all.remove(head[0])
@@ -56,8 +88,7 @@ if __name__ == "__main__":
                                             answer_4 = sum(answer_4,[])
                                             answer_4.sort()
                                             all_pure_one_color.append(answer_4)
-    # # 七対子
-    import itertools
+    # 七対子
     seven_pairs = []
     for pair in itertools.combinations(heads, 7):
         pair = sum(list(pair),[])
